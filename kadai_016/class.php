ae31d5762e6913614710da2fd9962506405d923a
gyouzamaru <ami1225.2000@gmail.com>
2024-09-04 01:04:08 +0900

    課題提出4

---
+++ kadai_016/class.php
@@ -0,0 +1,41 @@
+<?php
+ class Food {
+  private $name;
+  private $price;
+
+  public function __construct(string $name,int $price){
+   $this->name = $name;
+   $this->price = $price;
+ }
+ public function show_price() {
+  echo $this->price . '<br>';
+ }
+}
+
+ class Animal {
+  private $name;
+  private $height;
+  private $weight;
+
+  public function  __construct(string $name,int $height,int $weight){
+    $this->name = $name;
+    $this->height = $height;
+    $this->weight = $weight;
+  }
+  public function show_height(){
+    echo $this->height;
+  }
+ }
+
+$food = new food('potato',250);
+print_r($food);
+echo '<br>';
+
+$animal = new Animal('dog',60,5000);
+print_r($animal);
+echo '<br>';
+
+$food ->show_price();
+$animal ->show_height();
+
+?>
